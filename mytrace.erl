3fda24453cf0496a5c2891d09ceac8e3f4f0819a
Robert Fiko <fiko.robert@gmail.com>
2021-12-08 09:37:21 +0100

    Added trace for new modules

--- mytrace.erl
+++ mytrace.erl
@@ -1,5 +1,5 @@
 -module(mytrace).
--export([start_trace/2, trace_unused/1, trace_referl_els/1]).
+-export([start_trace/2, trace_unused/1, trace_referl_els/1, trace_referl_common/1]).
 
 trace_unused(Node) ->
     start_trace(Node,'els_unused_macros_referl_diagnostics').
@@ -8,6 +8,10 @@ trace_referl_els(Node) ->
     start_trace(Node, els_refactorerl_utils),
     start_trace(Node, els_referl_unused_macros_diagnostics),
     start_trace(Node, els_referl_unsecure_calls_diagnostics).
+
+trace_referl_common(Node) ->
+    start_trace(Node, els_refactorerl_utils),
+    start_trace(Node, els_refactorerl_diagnostics).
 
 start_trace(Node, Module) ->
     dbg:tracer(),
