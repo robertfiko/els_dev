f01a9fe61414405c3488ac35d9ff51f1d677cfb4
Robert Fiko <fiko.robert@gmail.com>
2021-12-08 09:37:10 +0100

    Remake script update

--- remake.sh
+++ remake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "UNINSTAL:====="
-code --uninstall-extension "/Users/fikorobert/ls//vscode/erlang-ls-0.0.30.vsix"
+code --uninstall-extension "/Users/fikorobert/ls//vscode/erlang-ls-0.0.32.vsix"
 
 echo "CD:====="
 cd /Users/fikorobert/ls-referl/vscode
@@ -12,7 +12,7 @@ echo "VSCE:====="
 /Users/fikorobert/.npm-global/lib/node_modules/vsce/out/vsce package
 
 echo "INSTALL:====="
-code --install-extension "/Users/fikorobert/ls/vscode/erlang-ls-0.0.30.vsix"
+code --install-extension "/Users/fikorobert/ls/vscode/erlang-ls-0.0.32.vsix" --force
 
 echo "DONE:====="
 
