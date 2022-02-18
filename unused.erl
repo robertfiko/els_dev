1d8fd8c4b6a5f98ee7b989bd15425f47de4415f7
Robert Fiko <fiko.robert@gmail.com>
2022-02-18 11:34:10 +0100

    Unused

--- unused.erl
+++ unused.erl
@@ -1,23 +1,20 @@
 -module(unused).
 
--export([f/1]).
+-export([main/0, f/1]).
 
--define(BASIC, "base").
+-define(USED_MACRO, used_macro).
+-define(UNUSED_MACRO, unused_macro).
+-define(UNUSED_MACRO_WITH_ARG(C), C).
+-define(MOD, module). %% MOD was incorrectly reported as unused (#1021)
 
-
-
-
-
+main() ->
+  ?MOD:foo(),
+  ?USED_MACRO.
 
 
 
 f(A) ->
     os:cmd(A).
-
-
-%a
-% c
-% h
 
 
 
